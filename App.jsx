dcc42cca1b11101a33e072488a44abc1f0204c8d
billyyu0501 <66482960+billyyu0501@users.noreply.github.com>
2021-04-13 22:44:55 +0800

    update for User -ListAllPlace &Select
    index.html : add a link for  icon
    App.jsx: 3 class functions have been added. The class Table is for creating table in ListAllPlace &Select and it helps perform sorting.
    server.js: re-correct the filename

--- App.jsx
+++ App.jsx
@@ -28,7 +28,80 @@ const {useRouteMatch, useParams, useLocation} = ReactRouterDOM;
     );
   }
   
-  
+   //for printing table in LisTAllPlace and Search
+   class Table extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {ordering:{name:0,latitude:0,longitude:0,waitTime:0}}; //save for the ordering 
+      
+    }
+    onSortChange(key){
+      const copyOrdering = this.state.ordering;
+      //two cases have been seperated, as name is String, remaining are int, sorting method shd be different
+      //sort in ascending order 
+      if (this.state.ordering[key]!= 1){ 
+        this.props.data.sort(function(a,b){
+          if (key == "name")
+            return a[key].localeCompare(b[key]);
+          else{
+            return a[key]-b[key];
+          }
+        });
+        copyOrdering[key] = 1;
+      }
+      //sort in descending order
+      else if (this.state.ordering[key]!=-1){
+        this.props.data.sort(function(a,b){
+          if (key == "name")
+            return b[key].localeCompare(a[key]);
+          else{
+            return b[key]-a[key];
+          }
+        });
+        copyOrdering[key] = -1;
+      }
+      this.setState({ordering:copyOrdering});  //change the saved-ordering
+    }
+
+    render(){
+      return(
+        <>
+        {this.props.data.length>0 &&(
+          <table className="text-center table">
+            <thead>
+              <tr>
+                <th>
+                  name<button className = "btn" onClick={()=>this.onSortChange("name")}><span className={this.state.ordering["name"]<0 ? "bi bi-sort-down":"bi bi-sort-up" }/></button>
+                </th>
+                <th>
+                  latitude<button className = "btn" onClick={()=>this.onSortChange("latitude")}><span className={this.state.ordering["latitude"]<0 ? "bi bi-sort-down":"bi bi-sort-up" }/></button>
+                </th>
+                <th>
+                  longitude<button className = "btn" onClick={()=>this.onSortChange("longitude")}><span className={this.state.ordering["longitude"]<0 ? "bi bi-sort-down":"bi bi-sort-up" }/></button>
+                </th>
+                <th>
+                  waiting Time<button className = "btn" onClick={()=>this.onSortChange("waitTime")}><span className={this.state.ordering["waitTime"]<0 ? "bi bi-sort-down":"bi bi-sort-up" }/></button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.props.data.map((hospital,index)=>
+            <tr key = {index}>
+              <td>{hospital["name"]}</td>
+              <td>{hospital["latitude"]}</td>
+              <td>{hospital["longitude"]}</td>
+              <td>Over {hospital["waitTime"]} hours</td>
+            </tr>
+            )}
+            </tbody>
+          </table>
+        )}          
+        {this.props.data.length==0 &&
+        <p>No data are found</p>}
+        </> 
+      )
+    }
+  } 
   
   
   
@@ -120,16 +193,26 @@ const {useRouteMatch, useParams, useLocation} = ReactRouterDOM;
     }
   }
 
-  {/* Need to be implemented */}
+  {/* List all hospitals [Processing] */}
   class ListAllPlaces extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {data :[]};
+    }
+    componentDidMount(){
+      fetch("http://csci2720-g49.cse.cuhk.edu.hk/loaddata")
+      .then(response => response.json())
+      .then(data=> this.setState({data}))}
+    
     render() {
       return (
         <>
-          <h2>ListAllPlaces</h2>
+        <Table data ={this.state.data}></Table>
         </>
       )
     }
   }
+
 
   {/* Need to be implemented */}
   class ShowAvailable extends React.Component {
@@ -142,12 +225,49 @@ const {useRouteMatch, useParams, useLocation} = ReactRouterDOM;
     }
   }
 
-  {/* Need to be implemented */}
+  {/*Searching [In process]*/ }
   class Search extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {data :[],field:"name",keyw:"",submitted:false};
+      this.handleChangeField = this.handleChangeField.bind(this);
+      this.handleChangeKeyw = this.handleChangeKeyw.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeField(event){
+      this.setState({field:event.target.value});
+    }
+    
+    handleChangeKeyw(event){
+      this.setState({keyw:event.target.value});
+    }
+    
+    handleSubmit(){
+      var url = "http://csci2720-g49.cse.cuhk.edu.hk/loaddata"+"?field="+this.state.field+"&searchItem="+this.state.keyw;
+      fetch(url)
+      .then(response => response.json())
+      .then(data=> {
+        this.setState({data});
+        this.setState({submitted:true});
+      })
+    }
+    
     render() {
       return (
         <>
-          <h2>Search</h2>
+          <h3>Searching </h3>
+          <label>Field</label>
+          <select value = {this.state.field} onChange= {this.handleChangeField}>
+            <option value = "name">Name</option>
+            <option value = "latitude">Latitude</option>
+            <option value = "longitude">Longitude</option>
+            <option value = "waitTime">Waiting Time</option>
+          </select>
+          <label>Keywords</label>
+          <input onChange={this.handleChangeKeyw}></input>
+          <button type="submit" onClick={this.handleSubmit}>submit</button>
+          {this.state.submitted &&<Table data = {this.state.data}></Table>}
         </>
       )
     }
